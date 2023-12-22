1aa6d6b2efc908c2757747e27317c76831ea88ed
Yaroslav-Korzh <dunny04@gmail.com>
2023-12-22 16:43:41 +0100

    First commit

--- src/main/java/org/example/Meth.java
+++ src/main/java/org/example/Meth.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Meth {
     public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+
         String result = strs[0];
         for (int i = 0; i < strs.length; i++) {
             while (strs[i].indexOf(result) != 0) {
