65e897ca8f9814304b0802cddbd008202758357c
Yaroslav-Korzh <dunny04@gmail.com>
2023-12-22 16:36:06 +0100

    First commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import java.util.Vector;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] strs = {"flower","flow","flight"};
+        Meth meth = new Meth();
+        System.out.println(meth.longestCommonPrefix(strs));
     }
 }
---
+++ src/main/java/org/example/Meth.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Meth {
+    public String longestCommonPrefix(String[] strs) {
+        String result = strs[0];
+        for (int i = 0; i < strs.length; i++) {
+            while (strs[i].indexOf(result) != 0) {
+                result = result.substring(0, result.length() -1);
+                if (result.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+        return result;
+    }
+}
